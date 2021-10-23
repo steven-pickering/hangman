79f18346cc857f5bd37ab63813bd331dbb7d834f
Steven Pickering <52013044+Steven909@users.noreply.github.com>
2021-10-23 21:42:36 +0100

    Adding WORDS ARRAY

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,16 @@ package application;
 public class App {
 
     public static void main(String[] args) {
+        System.out.println();
+        String[] words = {"latter", "gentleman", "had", "made", "half", "holiday", "on", "purpose", "thus", "gaining\\n " +
+                "immense", "advantage", "over", "the", "youngest", "gentleman", "rest", "whose\\n" +
+                "time", "perversely", "chanced", "bespoke", "until", "the", "evening\n" +
+                "bottle", "wine", "midst", "enjoyment"};
 
 
+        String stickman1 = "   0_______";
+        String stickman2 = "__|__    /'\'";
+        String stickman3 = "   |     /  '\'";
+        String stickman4 = "  / '\'   /    '\'";
     }
 }
--- src/application/Hangman.java
+++
@@ -1,8 +0,0 @@
-package application;
-
-public class Hangman {
-    private String word;
-    private int numberOfLetters = 0;
-    private String letter;
-
-}
